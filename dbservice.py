d61d71cb07deee0b13471e0092a4d4ef50cdcc8a
nyaswabuhh <alex.navitech@gmail.com>
2025-06-30 18:14:03 +0300

    sales

---
+++ dbservice.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app= Flask (__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:simbapos%402019@localhost:5432/flask_api'
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    __tablename__='products'
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False)
+    bp=db.Column(db.Float,nullable=False)
+    sp=db.Column(db.Float, nullable=False)
+
+    sales=db.relationship("Sale", backref="product")
+
+class Sale(db.Model):
+    __tablename__="sales"
+    id=db.Column(db.Integer, primary_key=True)
+    pid=db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    quantity=db.Column(db.Integer, nullable=False)
+    created_at=db.Column(db.DateTime, default=datetime.now, nullable=False)
+
+with app.app_context():
+    db.create_all()
+    
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, jsonify, request
+from dbservice import Product, Sale,db,app
 
-app=Flask(__name__)
 
-product_list=[]
+
+# app=Flask(__name__)
+
+# product_list=[]
+
 
 @app.route("/")
 def index():
@@ -13,7 +17,19 @@ def index():
 @app.route("/api/products", methods=["GET","POST"])
 def products():
     if request.method=="GET":
-        return jsonify(product_list),200
+        product_list =Product.query.all()
+        print ('product list', product_list)
+
+        prods=[]
+        for prod in product_list:
+            product_data={
+                "id":prod.id,
+                "name":prod.name,
+                "bp":prod.bp,
+                "sp":prod.sp
+            }
+            prods.append(product_data)
+        return jsonify(prods), 200
     elif request.method=="POST":
         data = dict(request.get_json())
         if "name" not in data.keys()or "bp" not in data.keys() or "sp" not in data.keys():
@@ -25,11 +41,54 @@ def products():
             return jsonify(error),401
 
         else:
-            product_list.append(data)
+            # product_list.append(data)
+            new_product=Product(name=data["name"], bp=data["bp"], sp=data["sp"])
+            db.session.add(new_product)
+            db.session.commit()
             return jsonify(data), 201
     else:
         error = {"error": "Method not allowed"}
         return jsonify(error),401
+    
+
+@app.route("/api/sales", methods=["GET", "POST"])
+def sales():
+    if request.method=="GET":
+        sold_items =Sale.query.all()
+        print ('sold items', sold_items)
+
+        sold=[]
+        for i in sold_items:
+            sold_items_data={
+                "id":i.id,
+                "pid":i.pid,
+                "quantity":i.quantity,
+                "created_at":i.created_at
+            }
+            sold.append(sold_items_data)
+        return jsonify(sold), 200
+    
+    elif request.method=="POST":
+        data = dict(request.get_json())
+        if "pid" not in data.keys()or "quantity" not in data.keys():
+            error={"error":"Invalid Keys"}
+            return jsonify(error),401
+
+        elif data["pid"]=="" or data["quantity"]=="":
+            error={"error":"Ensure all fields are filled"}
+            return jsonify(error),401
+        
+        else:
+            
+            new_sale=Sale(pid=data["pid"], quantity=data["quantity"])
+            db.session.add(new_sale)
+            db.session.commit()
+            return jsonify(data), 201
+
+    else:
+        error={"error":"Method not allowed"}
+        return jsonify(error), 201
+
        
 app.run(debug=True)
 
