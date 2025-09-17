f7718ee55bd952f3b3bdc43a884e7efde5be890b
nyaswabuhh <alex.navitech@gmail.com>
2025-09-17 20:50:05 +0300

    cleanup

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from flask import Flask, json, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from dbservice import Payment, Product, Sale,User,db,app
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 # import sentry_sdk
